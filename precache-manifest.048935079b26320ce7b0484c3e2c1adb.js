37334ddfeeae6f4748d6e6249b4202b430eb5d82
Royalgiraffe <royalgiraffe1@gmail.com>
2020-12-13 14:07:27 -0800

    Updates

--- precache-manifest.048935079b26320ce7b0484c3e2c1adb.js
+++ precache-manifest.048935079b26320ce7b0484c3e2c1adb.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "90d0cc9c18b89401f581c1f89d8f8a06",
+    "revision": "c38e5f8eea01d3bf89d46634cb8726e4",
     "url": "/legacy-sim/index.html"
   },
   {
-    "revision": "77649fa2698fbcd3e56c",
+    "revision": "6d8c5dc50dc6f47f1ed4",
     "url": "/legacy-sim/static/css/main.1cd6dc54.chunk.css"
   },
   {
-    "revision": "4652e641fb02b47e4fc2",
-    "url": "/legacy-sim/static/js/2.d6f095ab.chunk.js"
+    "revision": "b1cc862e9362dc966c31",
+    "url": "/legacy-sim/static/js/2.4d3790d5.chunk.js"
   },
   {
-    "revision": "77649fa2698fbcd3e56c",
-    "url": "/legacy-sim/static/js/main.97330da7.chunk.js"
+    "revision": "6d8c5dc50dc6f47f1ed4",
+    "url": "/legacy-sim/static/js/main.084243b9.chunk.js"
   },
   {
     "revision": "7353e42fc75985912ece",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/legacy-sim/precache-manifest.e6551da8b3d02b9b37f7841c0af65e54.js"
+  "/legacy-sim/precache-manifest.048935079b26320ce7b0484c3e2c1adb.js"
 );
 
 self.addEventListener('message', (event) => {
